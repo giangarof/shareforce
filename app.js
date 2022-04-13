e2cb71a368ee857539fb0db2399d39e1e1860c0b
Gianmarco G <giangarof98@gmail.com>
2022-04-13 17:15:42 -0400

    crud

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 
 const db = require('./mongo/connection');
 const Post = require('./models/modelPost');
@@ -9,15 +10,49 @@ const PORT = process.env.port || 3000;
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req,res) => {
     res.render('home');
 });
 
-app.get('/post', async(req,res) => {
-    const post = new Post ({title: 'first another'})
+app.get('/posts', async (req,res) => {
+    const posts = await Post.find({});
+    res.render('posts', { posts });
+});
+
+app.get('/posts/new', (req,res) => {
+    res.render('new')
+})
+
+app.post('/posts', async (req,res) => {
+    const post = new Post(req.body.posts)
     await post.save();
-    res.send(post)
+    res.redirect(`/posts/${post._id}`)
+})
+
+app.get('/posts/:id', async (req,res) => {
+    const posts = await Post.findById(req.params.id);
+    res.render('show', { posts });
+})
+
+app.get('/posts/:id/update', async (req, res) => {
+    const posts = await Post.findById(req.params.id);
+    res.render('edit', { posts });
+})
+
+app.put('/posts/:id', async (req,res) => {
+    const {id} = req.params;
+    const post = await Post.findByIdAndUpdate(id, {...req.body.posts});
+    res.redirect(`/posts/${post._id}`)
+})
+
+app.delete('/posts/:id', async (req,res) => {
+    const {id} = req.params;
+    const post = await Post.findByIdAndDelete(id);
+    res.redirect('/posts');
 })
 
 app.listen(PORT, () => {
