ce90690dede71051f1e287855c755f2106dd7e05
Gianmarco G <giangarof98@gmail.com>
2022-04-15 20:51:01 -0400

    static assets

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/posts', postRoutes);
 app.use('/posts/:id/review', reviewRoutes);
